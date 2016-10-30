9e2e9d5d8771cafff4706b210a013ebe7c85d775
MaximeGenevier <maxime.genevier.sio@gmail.com>
2016-10-30 20:43:01 +0100

    Ajout du projet PC

---
+++ Puissance-desktop/src/com/puissance/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.puissance.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.puissance.game.PuissanceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PuissanceGame(), config);
+	}
+}
