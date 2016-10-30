efb8f26344405e524172f9730dacf9c21cdc5f44
MaximeGenevier <maxime.genevier.sio@gmail.com>
2016-10-30 20:44:06 +0100

    Update

--- Puissance-desktop/src/com/puissance/game/desktop/DesktopLauncher.java
+++ Puissance-desktop/src/com/puissance/game/desktop/DesktopLauncher.java
@@ -3,7 +3,11 @@ package com.puissance.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.puissance.game.PuissanceGame;
-
+/**
+ * 
+ * @author Maxime Genevier
+ *
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
